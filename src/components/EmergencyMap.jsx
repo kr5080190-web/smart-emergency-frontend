31fb1c49bed7b5be4a944ecc54b0ec864982dc3d
Archana <yourgmail@gmail.com>
2026-05-07 14:30:59 +0530

    fixed vercel map deployment

--- src/components/EmergencyMap.jsx
+++ src/components/EmergencyMap.jsx
@@ -2,6 +2,17 @@ import React, { useEffect } from 'react';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl:
+    'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png',
+  iconUrl:
+    'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png',
+  shadowUrl:
+    'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png',
+});
+
 function EmergencyMap({ userLocation }) {
   useEffect(() => {
     if (!userLocation) return;
@@ -11,25 +22,23 @@ function EmergencyMap({ userLocation }) {
       13
     );
 
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; OpenStreetMap contributors',
-    }).addTo(map);
+    L.tileLayer(
+      'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+      {
+        attribution: '© OpenStreetMap contributors',
+      }
+    ).addTo(map);
 
     L.marker([
       userLocation.latitude,
       userLocation.longitude,
-    ])
-      .addTo(map)
-      .bindPopup('You are here')
-      .openPopup();
+    ]).addTo(map);
 
     setTimeout(() => {
       map.invalidateSize();
     }, 100);
 
-    return () => {
-      map.remove();
-    };
+    return () => map.remove();
   }, [userLocation]);
 
   return (
@@ -38,10 +47,9 @@ function EmergencyMap({ userLocation }) {
       style={{
         height: '400px',
         width: '100%',
-        borderRadius: '12px',
         marginTop: '20px',
       }}
-    />
+    ></div>
   );
 }
 
